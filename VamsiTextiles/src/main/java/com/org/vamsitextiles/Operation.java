e61ff534d2508c0ebef7d704835cd545822b73e1
vamsibaisani <baisanikrish005@gmail.com>
2022-11-01 10:00:48 +0530

    updated...

--- VamsiTextiles/src/main/java/com/org/vamsitextiles/Operation.java
+++ VamsiTextiles/src/main/java/com/org/vamsitextiles/Operation.java
@@ -1,15 +1,19 @@
 package com.org.vamsitextiles;
 
 public class Operation {
-	
+
 	public float add(float a, float b) {
 		// logic
-		return  (a+b);
+		return (a + b);
 	}
 
 	public float sub(float a, float b) {
 		// logic
-		return  a-b;
+		return a - b;
+	}
+
+	public float mul(float a, float b) {
+		return a * b;
 	}
 
 }
